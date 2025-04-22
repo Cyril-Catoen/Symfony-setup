9ebd63b43a78f92124be5c7b98ab3125e3a442fa
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-04-22 16:31:10 +0200

    ajout page list articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 // Création de la classe HomeController
 class ArticleController extends AbstractController {
 
@@ -17,13 +18,41 @@ class ArticleController extends AbstractController {
 	// Quand un utilisateur demande l'url "/", la fonction est appelée
 	#[Route('/list-articles', name:"list-articles")]
 	public function listArticles() {
+
+        $articles = [
+            1 => [
+                "id" => 1,
+                "title" => "Article 1",
+                "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tenetur repellendus quod voluptates nemo amet voluptas pariatur, molestias magnam ab cumque quas deserunt, voluptate, nobis iste quae dolores? Debitis, sunt.",
+                "created_at" => new \DateTime(),
+                "image" => "https://cdn.pixabay.com/photo/2018/08/04/11/30/draw-3583548_1280.png",
+                "is_published" => true
+            ],
+
+            2 => [
+                "id" => 2,
+                "title" => "Article 2",
+                "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tenetur repellendus quod voluptates nemo amet voluptas pariatur, molestias magnam ab cumque quas deserunt, voluptate, nobis iste quae dolores? Debitis, sunt.",
+                "created_at" => new \DateTime(),
+                "image" => "https://cdn.pixabay.com/photo/2018/08/04/11/30/draw-3583548_1280.png",
+                "is_published" => false
+            ],
+            3 => [
+                "id" => 3,
+                "title" => "Article 3",
+                "content" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur tenetur repellendus quod voluptates nemo amet voluptas pariatur, molestias magnam ab cumque quas deserunt, voluptate, nobis iste quae dolores? Debitis, sunt.",
+                "created_at" => new \DateTime(),
+                "image" => "https://cdn.pixabay.com/photo/2018/08/04/11/30/draw-3583548_1280.png",
+                "is_published" => true
+            ]
+        ];
         // return new Response("<p>Liste</p>", 200);
         
         // j'utilise la méthode render
 		// qui permet de récupérer un fichier de views twig
 		// de le transformer en HTML
 		// et de la renvoyer en response HTTP avec un status 200
-		return $this->render('list-articles.html.twig');
+		return $this->render('list-articles.html.twig', ['articles' => $articles]);
 	}
 
 	#[Route('/create-articles', name:"create-articles")]
