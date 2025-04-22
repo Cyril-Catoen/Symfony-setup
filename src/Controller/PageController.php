3652e3d59ab1c70f28ee5acb38d97843a28111a3
Cyril-Catoen <cyril.catoen@lapiscine.pro>
2025-04-22 13:52:01 +0200

    router

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -6,7 +6,7 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 
 // Création de la classe HomeController
-class HomeController {
+class PageController {
 
 
 	// Ajout d'une fonction nommée Home (méthode) qui exécute un var_dump
@@ -15,6 +15,16 @@ class HomeController {
 	#[Route('/', name:"home")]
 	public function home() {
 		var_dump('Page accueil'); die;
+	}
+
+	#[Route('/contact', name:"contact")]
+	public function contact() {
+		var_dump('Page de contact'); die;
+	}
+
+	#[Route('/a-propos', name:"a-propos")]
+	public function propos() {
+		var_dump('Page à propos de'); die;
 	}
 
 }
